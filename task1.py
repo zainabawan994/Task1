195a794ae21c7a807d05c502566d80b85c16c992
Zainab BiBi <zainabawan3316@gmail.com>
2025-07-04 17:46:26 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+## Your name , Your Hobby , Age  & Why you want to learn Python.
+
+print(" My Name is  Zainab ")
+print("My hobby is Reading books")
+print("I want to be a Data Scientist so that's why I am learning Python")
+
+## Ask the user to input two numbers, Print the result of their addition.
+number1=int(input("Enter the first number: "))
+number2=int(input("Enter the second number: "))
+addition=number1+number2
+print("The addition of", number1, "and", number2, "is:", addition)
